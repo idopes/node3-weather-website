46394988f50f8b259aabd7a97941927fde531cf4
Ido Samet <idosamet@Idos-MacBook-Pro.local>
2020-03-27 17:03:03 +0300

    Adding forecast text

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,21 +1,23 @@
-const request = require('request');
+const request = require("request");
 
 const forecast = (latitude, longitude, callback) => {
-    const url = `https://api.darksky.net/forecast/f78c7817581c4c208e5dbbe6a5a5ec6e/${latitude},${longitude}?units=si`;
+  const url = `https://api.darksky.net/forecast/f78c7817581c4c208e5dbbe6a5a5ec6e/${latitude},${longitude}?units=si`;
 
-    request({ url, json: true }, (error, { body }) => {
-        if (error) {
-            callback('Unable to connect to weather service!');
-        } else if (body.error) {
-            callback('Unable to find location');
-        }
-
-        else {
-            const { temperature, precipProbability } = body.currently;
-            const { summary } = body.daily.data[0];
-            callback(undefined, `${summary} It is currently ${temperature} degrees out. There is a ${precipProbability}% chance of rain`);
-        }
-    });
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to weather service!");
+    } else if (body.error) {
+      callback("Unable to find location");
+    } else {
+      const { temperature, precipProbability } = body.currently;
+      const { summary, temperatureHigh, temperatureLow } = body.daily.data[0];
+      console.log(body.daily.data);
+      callback(
+        undefined,
+        `${summary} It is currently ${temperature} degrees out. There is a ${precipProbability}% chance of rain. Highest temperature for the day is ${temperatureHigh} and lowest will be ${temperatureLow}`
+      );
+    }
+  });
 };
 
 module.exports = forecast;
