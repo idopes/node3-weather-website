755f17849129c139c0e74f30d93f7e7e67759c61
Ido Samet <idosamet@Idos-MacBook-Pro.local>
2020-03-27 16:37:01 +0300

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -1,101 +1,106 @@
-const path = require('path');
-const express = require('express');
-const hbs = require('hbs');
-const geoCode = require('./utils/geoCode');
-const forecast = require('./utils/forecast');
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
+const geoCode = require("./utils/geoCode");
+const forecast = require("./utils/forecast");
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define path for Express config
-const publicDirectory = path.join(__dirname, '../public');
-const viewsPath = path.join(__dirname, '../templates/views');
-const partialPath = path.join(__dirname, '../templates/partials');
-
+const publicDirectory = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialPath = path.join(__dirname, "../templates/partials");
 
 // Setup handlebars engine and views location
-app.set('view engine', 'hbs');
-app.set('views', viewsPath);
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
 hbs.registerPartials(partialPath);
 
 // Setup static directory to serve
 app.use(express.static(publicDirectory));
 
-app.get('', (req, res) => {
-    res.render('index', {
-        title: "Weather App!",
-        name: "Ido Samet"
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather App!",
+    name: "Ido Samet"
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About me",
+    name: "Ido Samet"
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "help",
+    name: "Ido Samet",
+    helpText: "this is a help message"
+  });
+});
+
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "You must provide a address"
     });
-});
+  }
 
-app.get('/about', (req, res) => {
-    res.render('about',
-        {
-            title: "About me", name: "Ido Samet"
-        });
-});
-
-app.get('/help', (req, res) => {
-    res.render('help', {
-        title: "help",
-        name: "Ido Samet",
-        helpText: "this is a help message"
-    });
-});
-
-app.get('/weather', (req, res) => {
-    if (!req.query.address) {
+  geoCode(
+    req.query.address,
+    (error, { latitude, longitude, location } = {}) => {
+      if (error) {
         return res.send({
-            error: 'You must provide a address'
+          error: "You must provide a valid address"
         });
-    }
-
-    geoCode(req.query.address, (error, { latitude, longitude, location } = {}) => {
-        if (error) {
-            return res.send({
-                error: 'You must provide a valid address'
-            });
-        }
-        else {
-            forecast(latitude, longitude, (error, data) => {
-                error ? res.send({
-                    error: 'You must provide a address'
-                }) : res.send({ forecast: data, location, address: req.query.address });
-            });
-        }
-    });
-});
-
-app.get('/products', (req, res) => {
-    if (!req.query.search) {
-        return res.send({
-            error: 'You must provide a search term'
+      } else {
+        forecast(latitude, longitude, (error, data) => {
+          error
+            ? res.send({
+                error: "You must provide a address"
+              })
+            : res.send({
+                forecast: data,
+                location,
+                address: req.query.address
+              });
         });
+      }
     }
-    res.send({ product: [] });
-
+  );
 });
 
-app.get('/help/*', (req, res) => {
-
-    res.render('404', {
-        title: "Help Error Page",
-        name: "Ido Samet",
-        errorText: 'Help Doc not found'
+app.get("/products", (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: "You must provide a search term"
     });
+  }
+  res.send({ product: [] });
 });
 
-app.get('*', (req, res) => {
-    res.render('404', {
-        title: "Error Page",
-        name: "Ido Samet",
-        errorText: 'Page not found'
-    });
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    title: "Help Error Page",
+    name: "Ido Samet",
+    errorText: "Help Doc not found"
+  });
 });
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.');
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "Error Page",
+    name: "Ido Samet",
+    errorText: "Page not found"
+  });
 });
 
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}.`);
+});
 
 // app.com
 // app.com/help
